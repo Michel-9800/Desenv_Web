4ea9e794eeb94e218cacca76eb11de4389ab2ce6
Michel-9800 <36209495+Michel-9800@users.noreply.github.com>
2018-04-03 17:35:12 -0300

    Add arquivos via upload

--- Desenv_Web/Desenv_Web/urls.py
+++ Desenv_Web/Desenv_Web/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-
+from django.contrib import admin
+from Desenv_Web.views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
+path('', index),
+path('', contato),
+path('', login),
+path('admin/', admin.site.urls),
 ]
+
